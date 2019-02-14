c83fe28fea2c0154afb7d3648c92317aaec4ec81
Jeffrey Lansford <lansfordjr14@gmail.com>
2019-02-13 23:20:57 -0700

    Added initial scene with box moving on platform
    added .gitignore with cross platform compatibly

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public Rigidbody2D rgi;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgi = gameObject.GetComponent<Rigidbody2D>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if ( Input.GetKey("a") && !Input.GetKey("d") ) {
+            rgi.AddForce(new Vector2(-speed, 0));
+        }  
+      if (Input.GetKey("d") && !Input.GetKey("a")) {
+            rgi.AddForce(new Vector2(speed, 0));
+        }
+    }
+}
