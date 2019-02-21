a4051cdc5c7c7766179fb769be725b566ffab132
Jeffrey Lansford <lansfordjr14@gmail.com>
2019-02-20 20:00:29 -0700

    Added velocity limit and a temporary background

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    public Rigidbody2D rgi;
+    private Rigidbody2D rgi;
+    public GameObject mainCamera;
     public float speed;
+    public float maxSpeed;
+    private Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
         rgi = gameObject.GetComponent<Rigidbody2D>();
+        offset = mainCamera.transform.position - transform.position;
 
 
     }
@@ -23,6 +27,13 @@ public class playerController : MonoBehaviour
         }  
       if (Input.GetKey("d") && !Input.GetKey("a")) {
             rgi.AddForce(new Vector2(speed, 0));
+        }
+        mainCamera.transform.position = mainCamera.transform.position + offset; 
+
+    }
+    private void FixedUpdate() {
+        if ( rgi.velocity.magnitude > maxSpeed ) {
+            rgi.velocity = rgi.velocity.normalized * maxSpeed; 
         }
     }
 }
